a19a73592f7580327e35828661e3b89e6e8442ff
GeimeAlondra <geimealondra07@gmail.com>
2022-08-11 20:02:56 -0600

    primer commit

---
+++ src/main/java/com/empresa/hoteljapp/app/HotelJappApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.hoteljapp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelJappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelJappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empresa/hoteljapp/app/HotelJappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.hoteljapp.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelJappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
