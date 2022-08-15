5a6824373bbc45be2077bf0b8d0b66026b1333a5
scratch2167 <rn21001@itcha.edu.sv>
2022-08-14 21:12:08 -0600

    DAOS

--- src/main/java/com/empresa/hoteljapp/app/models/dao/IClienteDAO.java
+++ src/main/java/com/empresa/hoteljapp/app/models/dao/IClienteDAO.java
@@ -1,5 +1,15 @@
 package com.empresa.hoteljapp.app.models.dao;
 
-public interface IClienteDAO {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import com.empresa.hoteljapp.app.models.entities.Cliente;
+
+public interface IClienteDAO extends CrudRepository<Cliente,Long>{
+	
+	@Query("FROM Cliente c WHERE c.nombre=:#{#cliente.nombre} and c.telefono=:#{#cliente.telefono}")
+	List<Cliente> findByNombreTelefono(Cliente cliente);
 
 }
--- src/main/java/com/empresa/hoteljapp/app/models/dao/IHabitacionDAO.java
+++ src/main/java/com/empresa/hoteljapp/app/models/dao/IHabitacionDAO.java
@@ -1,5 +1,12 @@
 package com.empresa.hoteljapp.app.models.dao;
 
-public interface IHabitacionDAO {
+import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
+
+import com.empresa.hoteljapp.app.models.entities.Habitacion;
+
+
+public interface IHabitacionDAO extends CrudRepository<Habitacion,Long>{
+	List<Habitacion> findByNombreIgnoreCase(String cadena);
 }
