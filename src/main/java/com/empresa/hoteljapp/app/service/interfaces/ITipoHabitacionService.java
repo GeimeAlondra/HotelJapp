36ac798ecd6365a0c79c14662e8c460ef348d414
scratch2167 <rn21001@itcha.edu.sv>
2022-08-20 15:35:11 -0600

    TipoHabitacion y Servicio

--- src/main/java/com/empresa/hoteljapp/app/service/interfaces/ITipoHabitacionService.java
+++ src/main/java/com/empresa/hoteljapp/app/service/interfaces/ITipoHabitacionService.java
@@ -1,5 +1,15 @@
 package com.empresa.hoteljapp.app.service.interfaces;
 
+import java.util.List;
+
+import com.empresa.hoteljapp.app.models.entities.TipoHabitacion;
+
+
 public interface ITipoHabitacionService {
+	public List<TipoHabitacion> findAll();
+	public TipoHabitacion findById(Long id);
+	public TipoHabitacion save(TipoHabitacion tipoHabitacion);
+	public void delete(Long id);
+	public List<TipoHabitacion> fyndByNombre(String nombre);
 
 }
